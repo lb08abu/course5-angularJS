f62ec2bc1d4ca7457b7a7a85bf38518a30bdc3d4
lb08abu <belcher_lewis@yahoo.com>
2016-09-12 00:16:16 +0200

    Updated functionality

--- assignments/assignment1/app.js
+++ assignments/assignment1/app.js
@@ -4,27 +4,23 @@
   angular.module("LunchCheck", [])
   .controller("LunchCheckController", LunchCheckController);
 
-  LunchCheckController.$inject = ["$scope", "$filter"]
-  function LunchCheckController($scope, $filter) {
+  LunchCheckController.$inject = ["$scope"]
+  function LunchCheckController($scope) {
     $scope.food = "";
     $scope.message = "";
-    $scope.warn = false;
-    $scope.error = false;
+    $scope.n_items = 0;
+    $scope.checked = false;
 
     $scope.checkLunch = function() {
-      var n_items = countInput(parseInput($scope.food));
+      $scope.checked = true;
+      $scope.n_items = countInput(parseInput($scope.food));
 
-      if (n_items <= 0) {
+      if ($scope.n_items <= 0) {
         $scope.message = "Please enter data first";
-        $scope.warn = true;
-        $scope.error = false;
-      } else if (n_items <= 3) {
+      } else if ($scope.n_items <= 3) {
         $scope.message = "Enjoy!";
-        $scope.warn = $scope.error = false;
-      } else if (n_items > 3) {
+      } else if ($scope.n_items > 3) {
         $scope.message = "Too much!";
-        $scope.error = true;
-        $scope.warn = false;
       } else {
         console.error("Weird input in food..");
       }
@@ -38,10 +34,10 @@
   function countInput(array) {
     var n = 0;
     for (var i = 0; i < array.length; i++) {
-      if (array[i] !== "") {
+      if (array[i].trim() !== "") {
         n++;
       }
     }
     return n;
-  }
+  };
 }());  // IIFE
